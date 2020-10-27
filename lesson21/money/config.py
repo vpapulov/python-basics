6c99b8ef00cf4a0362851914f1eab3562ac974b5
Vladlen Papulov <vladlen.papulov@gmail.com>
2020-10-27 22:11:55 +0300

    неработающая попытка .env файла

--- lesson21/money/config.py
+++ lesson21/money/config.py
@@ -1,5 +1,7 @@
 import os
 
 PG_HOST = os.environ["PG_HOST"]
+PG_USER = os.environ["PG_USER"]
+PG_PWD = os.environ["PG_PWD"]
 
-SQLALCHEMY_DATABASE_URI = f"postgres+psycopg2://user:pass@{PG_HOST}:5432/money"
+SQLALCHEMY_DATABASE_URI = f"postgres+psycopg2://{PG_USER}:{PG_PWD}@{PG_HOST}:5432/money"
