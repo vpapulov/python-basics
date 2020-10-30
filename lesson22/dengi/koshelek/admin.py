8ef4d557c2ddc02f7f736c8324d7c1577f5f673e
Vladlen Papulov <vladlen.papulov@gmail.com>
2020-10-30 19:58:38 +0300

    дз, урок 23

--- lesson22/dengi/koshelek/admin.py
+++
@@ -1,5 +0,0 @@
-from django.contrib import admin
-from .models import Account, MoneyIncome
-
-admin.site.register(Account)
-admin.site.register(MoneyIncome)
--- lesson22/dengi/koshelek/apps.py
+++
@@ -1,5 +0,0 @@
-from django.apps import AppConfig
-
-
-class KoshelekConfig(AppConfig):
-    name = 'koshelek'
